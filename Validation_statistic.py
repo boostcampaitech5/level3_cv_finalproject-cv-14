ea1ace4bbe53a1799de15aeb1563d8087a3dda2c
이종휘_T5153 <88420734+gndldl@users.noreply.github.com>
2023-07-27 20:22:15 +0900

    build: final version

--- Validation_statistic.py
+++ Validation_statistic.py
@@ -18,6 +18,7 @@ from torchvision.transforms import Resize
 from Embedding import Embedding_vector, Embeddings_Manager
 from Label_DataFrame import Label_DataFrame
 from sklearn.metrics import confusion_matrix
+from backbones import get_model, iresnet50, vit
 
 def calculate_mean_std(df) :
     p_mean = round(df[df.decision == "Yes"].distance.mean(), 4)
@@ -30,6 +31,21 @@ def calculate_mean_std(df) :
 
 def get_threshold(p_mean, p_std, sigma=1) :
     threshold = round(p_mean + sigma * p_std, 4)
+    return threshold
+
+def fine_tuning_threshold(model_df : Label_DataFrame,df, sigma=1) :
+    p_mean, p_std, n_mean, n_std = calculate_mean_std(df)
+    start = p_mean
+    end = n_mean
+    ths = np.arange(start, end, 0.001)
+    accuracy = 0
+    threshold = start
+    for t in ths :
+        prediction_df = model_df.get_prediction_df(threshold=t)
+        acc, recall, f1, precision = get_statistic(prediction_df)
+        if accuracy < acc :
+            accuracy = acc
+            threshold = t
     return threshold
 
 def get_statistic(df) :
@@ -61,7 +77,7 @@ def validation(model, vali_data_loader, test_path) :
     model_inference_df = model_df.get_inference_df(model_path2embedding)
 
     p_mean, p_std, n_mean, n_std = calculate_mean_std(model_inference_df)
-    threshold = get_threshold(p_mean, p_std, sigma=1)
+    threshold = fine_tuning_threshold(model_df,model_inference_df, sigma=1)
     facenet_prediction_df = model_df.get_prediction_df(threshold=threshold)
 
     accuracy, recall, f1, precision = get_statistic(facenet_prediction_df)
@@ -69,22 +85,35 @@ def validation(model, vali_data_loader, test_path) :
     return accuracy, recall, f1, precision
 
 if __name__ == '__main__' :
-    model_path = '/opt/ml/insightface/recognition/arcface_torch/work_dirs/wf4m_r50_epoch20/model.pt'
-    train_path = '/opt/ml/data/celeb/cut_train'
-    test_path = '/opt/ml/data/celeb/cut_test' 
+    # vit-t
+    # model_path = '/opt/ml/project/insightface/recognition/arcface_torch/work_dirs/FaceNet-Finetuning/model.pt'
+    # vit-b
+    model_path = 'model_pth_dir'
+    train_path = 'train_data_dir'
+    test_path = 'test_data_dir' 
 
     transform = transforms.Compose([
         np.float32,
         transforms.ToTensor(),  # range [0, 255] -> [0.0, 1.0]
-        Resize((112, 112)),
+        Resize((160, 160)),
         fixed_image_standardization
     ])
     facenet = InceptionResnetV1(classify=False, pretrained='vggface2')
-
+    facenet.load_state_dict(torch.load(model_path))
+    
+    # vit-t
+    # model = get_model("vit_t_dp005_mask0")
+    # model.load_state_dict(torch.load(model_path))
+    
+    # vit-b
+    # model = get_model("vit_b_dp005_mask_005")
+    # model.load_state_dict(torch.load(model_path))
+    
     test_dataset = Crawling_Nomal_Dataset(test_path, transforms=transform)
-    test_data_loader = data.DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=2, drop_last=False)
+    test_data_loader = data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=2, drop_last=False)
     
     accuracy, recall, f1, precision = validation(model=facenet, vali_data_loader=test_data_loader, test_path=test_path)
+
 
 
 
