2c9e26accca9e41a6ebcb75b785133f55dd3f0d8
w-jnn <sdnmd98@naver.com>
2023-07-28 13:23:14 +0900

    feat : add final project, deploy_ml

---
+++ deploy_ml/app.py
@@ -0,0 +1,228 @@
+from flask import Flask, request, jsonify
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+import cv2
+import numpy as np
+from torchvision import transforms
+import requests
+from pymongo import MongoClient
+from flask_cors import CORS
+import logging
+import sys 
+from datetime import datetime
+import math
+
+app = Flask(__name__)
+CORS(app)
+app.config['DEBUG'] = True
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+# 모델 초기화
+detection_model = MTCNN(image_size=160, margin=0.6, min_face_size=20, thresholds=[0.6,0.7,0.7], factor= 0.709, keep_all=True) 
+single_detection_model = MTCNN(image_size=160, margin=0, min_face_size=20, thresholds=[0.6, 0.7, 0.7], factor=0.709)
+facenet_model = InceptionResnetV1(classify=False, pretrained='vggface2')
+threshold = 0.3046
+facenet_model.eval()
+
+# MongoDB 연결 설정
+client = MongoClient("")  # 여기에 MongoDB 연결 정보를 입력해주세요.
+# db = client["memory-studio"]  # 여기에 실제 사용할 데이터베이스 이름을 입력해주세요.
+collection = client['memory-studio']['users']  # 여기에 실제 사용할 컬렉션 이름을 입력해주세요.
+
+
+def save_to_mongodb(url, embedding, user_email):
+    try:
+        # 현재 시간을 가져와서 uploadTime 필드에 저장
+        current_time = datetime.now()
+        # 이미지 URL과 임베딩 벡터를 MongoDB에 저장
+        document  = {
+            'imagePath': url,
+            'embeddingVector': embedding,
+            'uploadTime': current_time
+        }
+        user = collection.find_one({'email': user_email})
+        if user:
+            # 이미 사용자가 존재하는 경우, 이미지 정보를 추가합니다.
+            collection.update_one({'email': user_email}, {'$push': {'imagePaths': document}})
+        else:
+            # 사용자가 존재하지 않는 경우, 에러를 반환합니다.
+            raise Exception("사용자가 존재하지 않습니다.")
+
+    except Exception as e:
+        print(f"MongoDB에 데이터 저장 중 오류 발생: {str(e)}")
+        # 사용자가 존재하지 않는 경우 에러를 반환합니다.
+        return str(e)
+    # 성공적으로 저장되었을 경우 None을 반환합니다.
+    return None
+
+def get_image_from_s3(url):
+    try:
+        # URL에서 이미지 데이터를 가져오기
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # 가져온 데이터를 NumPy 배열로 변환
+        image_np = np.asarray(bytearray(response.content), dtype=np.uint8)
+
+        # NumPy 배열을 OpenCV 이미지로 변환
+        img = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+
+        return img
+
+    except Exception as e:
+        print(f"Error loading image from URL: {str(e)}")
+        return None
+
+def get_embedding(model, image_path=None, feature=None) :
+    # image_path로 기입된 경우와 image자체로 기입된 경우를 나눈다.
+    if image_path is not None :
+        img = get_image_from_s3(image_path)
+    else : 
+        img = feature
+        
+    # 이미지의 형태(shape)를 확인하기 위해 디버그 출력을 추가합니다.
+    
+
+    # dataset을 거치지 않고 나온 이미지는 3차원이기 때문에 모델에 넣어줄 수 있게 4차원 변환
+    if len(img.shape) == 3:         
+        img = img.unsqueeze(0)
+        
+    print(f"이미지 형태: {img.shape}")   
+     
+    with torch.no_grad():  # Use torch.no_grad() to disable gradient computation
+        embedding = model(img)
+        embedding = embedding.detach().cpu().numpy()    # embedding 연산은 CPU로 진행하기 위해 변환
+
+    return embedding
+
+@app.route("/process_image", methods=["POST"])
+def process_image():
+    try:
+        data = request.json
+        print(f"data: {data}")
+        file_urls = data.get("fileUrls", [])
+        user_email = data.get("userEmail")
+
+        embeddings_dict = {}  # 이미지 경로와 임베딩 벡터를 저장할 딕셔너리
+        
+        # 이미지들에 대한 얼굴 탐지 및 임베딩 벡터 계산
+        for url in file_urls:
+            img = get_image_from_s3(url)
+            if img is None:
+                print(f"Cannot load image from S3: {url}")
+                continue
+
+            with torch.no_grad():
+                feature = detection_model(img)
+                if feature is None:
+                    print(f"Cannot detect face in the image: {url}")
+                    continue
+
+            # 임베딩 벡터 계산 및 저장
+            embedding = get_embedding(facenet_model, feature=feature)
+            embeddings_dict[url] = embedding.tolist()
+            
+            # 임베딩 벡터와 이미지 URL을 MongoDB에 저장
+            save_to_mongodb(url, embedding.tolist(), user_email)
+
+        return jsonify(embeddings_dict)
+
+    except Exception as e:
+        app.logger.error(f"Error processing images: {str(e)}")
+        return jsonify({'error': str(e)}), 500
+
+
+#//////////////////////////////////////////
+
+
+def distance(embeddings1, embeddings2, distance_metric=0):
+    if distance_metric==0:
+        # Euclidian distance
+        diff = np.subtract(embeddings1, embeddings2)
+        dist = np.sum(np.square(diff),1)
+    elif distance_metric==1:
+        # Distance based on cosine similarity
+        dot = np.sum(np.multiply(embeddings1, embeddings2), axis=1)
+        norm = np.linalg.norm(embeddings1, axis=1) * np.linalg.norm(embeddings2, axis=1)
+        similarity = dot / norm
+        dist = np.arccos(similarity) / math.pi
+    else:
+        raise 'Undefined distance metric %d' % distance_metric
+
+    return dist
+
+
+
+
+# 거리를 비교해서, 적절한 image를 set형태로 return
+def get_result (key, enrolled_DB, threshold, album : dict) :
+    key_image_set = set()
+    key_image_embedding = enrolled_DB[key]
+    for image in album.keys() :
+        embeddings = album[image] # (n, 512)
+        dis = distance(key_image_embedding, embeddings, 1)
+        result = dis <= threshold  # result [True, False, False] 
+        if any(result) :          # any => 하나라도 True가 있으면
+            key_image_set.add(image)
+
+    return key_image_set
+
+
+# 이미지 유사도 계산 엔드포인트
+@app.route('/search', methods=['POST'])
+def search():
+    try:
+        data = request.json
+        # print(f"data: {data}")
+        file_url = data.get("fileUrl")
+        user_email = data.get("userEmail")
+        userImagePaths = data.get("userImagePaths",[])
+        print(f"file_url: {file_url}")
+        print(f"user_email: {user_email}")
+        print(f"userImagePaths: {userImagePaths}")
+        
+        
+        # MongoDB에서 사용자 정보 조회
+        user = collection.find_one({'email': user_email})
+        if user:
+            # 이미 사용자가 존재하는 경우, 해당 사용자의 imagePaths 정보 반환
+            image_paths = user.get('imagePaths', [])
+            album_dict = {image_info['imagePath']: image_info['embeddingVector'] for image_info in image_paths}
+            # print(album_dict)
+            key_dict = {}  # 이미지 경로와 임베딩 벡터를 저장할 딕셔너리
+
+            # 이미지들에 대한 얼굴 탐지 및 임베딩 벡터 계산
+            img = get_image_from_s3(file_url)
+            if img is None:
+                print(f"Cannot load image from S3: {file_url}")
+
+            with torch.no_grad():
+                feature = single_detection_model(img)
+                if feature is None:
+                    print(f"Cannot detect face in the image: {file_url}")
+
+            # 임베딩 벡터 계산 및 저장
+            embedding = get_embedding(facenet_model, feature=feature)
+            key_dict[file_url] = embedding.tolist()
+            print(key_dict)
+
+            final_classification = get_result(file_url, key_dict, threshold, album_dict)
+            return jsonify({'result': list(final_classification)})
+        else:
+            # 사용자가 존재하지 않는 경우, 에러 반환
+            raise Exception("사용자가 존재하지 않습니다.")
+
+    except Exception as e:
+        app.logger.error(f"Error processing images: {str(e)}")
+        return jsonify({'error': str(e)}), 500
+
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+ 
+
+if __name__ == '__main__':
+    app.run()
---
+++ finalproject/server.js
@@ -0,0 +1,452 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const cors = require("cors");
+const FormData = require("form-data");
+const mongoose = require("mongoose");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const passportLocalMongoose = require("passport-local-mongoose");
+const passport = require("passport");
+const aws = require("aws-sdk");
+const dotenv = require("dotenv");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+const uuid4 = require("uuid4");
+const axios = require("axios");
+
+require("aws-sdk/lib/maintenance_mode_message").suppress = true;
+
+dotenv.config();
+
+app.use(express.static(__dirname + "/public"));
+// app.use(express.static("public"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// DB연결
+mongoose.connect(process.env.DB_URL, {
+  // useUnifiedTopology: true,
+  // useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log(`error on DB connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
+
+// DB session 저장
+var store = new MongoDBStore({
+  uri: process.env.DB_URL,
+  collection: "mySessions",
+});
+
+// Catch errors
+store.on("error", function (error) {
+  console.log(error);
+});
+
+app.use(
+  session({
+    secret: "dwdasjfu123@#@fc",
+    resave: true,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.set("view engine", "ejs");
+
+// aws region 및 자격증명 설정
+const s3 = new aws.S3({
+  region: process.env.AWS_S3_REGION,
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+});
+
+//회원가입 스키마 지정
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  // 사용자마다 이미지 정보를 저장하는 필드 추가
+  imagePaths: [
+    {
+      imagePath: String,
+      embeddingVector: [Number], // 임베딩 벡터는 숫자 배열로 저장합니다.
+      uploadTime: { type: Date, default: Date.now }, // 파일 업로드 시간 정보를 저장하는 필드
+    },
+  ],
+});
+// 이메일을 기준으로 자동으로 검사
+UserSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+
+const User = mongoose.model("User", UserSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// var db;
+// MongoClient.connect(process.env.DB_URL, function(err, client) {
+//     // 연결되면 할일
+//     if (err) return console.log(err);
+
+//     db = client.db("final-project");
+
+//     // db.collection("post").insertOne({ 이름: "John", 나이: 20 }, function (에러, 결과) {
+//     //   console.log("저장완료");
+//     // });
+
+// });
+
+// 확장자 검사 목록
+const allowedExtensions = [".png", ".jpg", ".jpeg"];
+
+const uploadImage = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req, file, callback) => {
+      // 콜백 함수 두 번째 인자에 파일명(경로 포함)을 입력
+      //const uploadDirectory = req.query.directory ?? ''
+      // 확장자 검사
+      const extension = path.extname(file.originalname).toLowerCase();
+      if (!allowedExtensions.includes(extension)) {
+        return callback(new Error("wrong extension"));
+      }
+      const userFolder = req.user.email;
+      const uniqueFileName = `${userFolder}/${Date.now()}_${uuid4()}${extension}`;
+      callback(null, uniqueFileName);
+    },
+    acl: "public-read-write",
+  }),
+  // 이미지 용량 제한 (5MB)
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+const uploadKeyImage = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req, file, callback) => {
+      // 콜백 함수 두 번째 인자에 파일명(경로 포함)을 입력
+      //const uploadDirectory = req.query.directory ?? ''
+      // 확장자 검사
+      const extension = path.extname(file.originalname).toLowerCase();
+      if (!allowedExtensions.includes(extension)) {
+        return callback(new Error("wrong extension"));
+      }
+      const userFolder = req.user.email;
+      const uniqueFileName = `${userFolder}/key/${Date.now()}_${uuid4()}${extension}`;
+      callback(null, uniqueFileName);
+    },
+    acl: "public-read-write",
+  }),
+  // 이미지 용량 제한 (5MB)
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+// localMiddleware 함수 작성
+const localMiddleware = (req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+};
+
+// 로그인 상태를 res.locals에 저장하여 템플릿에서 사용 가능하도록 함
+app.use(localMiddleware);
+
+// ...
+
+// 나머지 라우트 및 서버 설정 코드는 여기에 추가
+
+// ...
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on 8080");
+});
+
+app.use((req, res, next) => {
+  // 로그인 상태를 res.locals에 저장하여 템플릿에서 사용 가능하도록 함
+  res.locals.user = req.isAuthenticated();
+  console.log(res.locals.user);
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log(req.user);
+  res.render("index.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password, confirm_password } = req.body;
+
+  if (password !== confirm_password) {
+    // 비밀번호 확인이 일치하지 않으면 회원가입 중단
+    return res.render("register.ejs", { error: "비밀번호 확인이 일치하지 않습니다." });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      // 이미 등록된 이메일이면 회원가입 중단
+      return res.render("register.ejs", { error: "이미 사용 중인 이메일입니다." });
+    }
+
+    const user = new User({ name, email });
+    await User.register(user, password);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.redirect("register.ejs");
+  }
+});
+
+app.post("/checkEmailDuplication", async (req, res) => {
+  const { email } = req.body;
+
+  // 이메일 중복 체크
+  const existingUser = await User.findOne({ email });
+
+  // 중복 여부에 따라 JSON 응답 보냄
+  res.json({ isDuplicate: existingUser !== null });
+});
+
+// app.get("/login", (req, res) => {
+//   res.render("login.ejs");
+// });
+
+// 로그인을 처리하는 라우트에 passport.authenticate 미들웨어 사용
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/", // 인증 실패 시 다시 /login으로 리디렉션
+    successRedirect: "/", // 인증 성공 시 /로 리디렉션
+  }),
+  (req, res) => {
+    // 이 부분은 인증이 성공하면 실행되는 부분입니다. (실행되지 않을 가능성이 높습니다.)
+    // 만약 인증에 성공하면 successRedirect로 리디렉션되므로 이 부분은 실행되지 않을 것입니다.
+    // 따라서 로그인 성공 시 어떤 동작을 취하고 싶다면 successRedirect로 리디렉션된 경로에서 처리해야 합니다.
+  }
+);
+
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    // 로그아웃 성공 시 알림 창을 띄우고 메인 페이지로 리디렉션
+    res.send('<script>alert("로그아웃 되었습니다."); window.location.href = "/";</script>');
+  });
+});
+
+app.get("/upload", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("upload.ejs");
+  } else {
+    // 로그인되어 있지 않은 상태라면 알림창을 띄우고 로그인 페이지로 리디렉션
+    res.send('<script>alert("로그인이 필요합니다."); window.location.href = "/";</script>');
+  }
+});
+
+app.post("/upload", uploadImage.array("file_object"), async (req, res, next) => {
+  const fileUrls = req.files.map((file) => file.location); // 업로드된 파일들의 경로 정보는 req.files에 저장됨
+  console.log(fileUrls); // 업로드된 파일들의 S3 경로를 출력
+
+  // Flask API의 URL
+  const FLASK_API_URL = "http://101.101.219.62:30006/process_image"; // 적절한 Flask API의 URL로 변경
+
+  // POST 요청 보내기
+  axios
+    .post(FLASK_API_URL, {
+      fileUrls: fileUrls,
+      userEmail: req.user.email,
+    })
+    .then(function (response) {
+      console.log("Flask 애플리케이션으로부터 받은 응답:", response.data);
+      res.status(200).json({ message: "업로드가 완료되었습니다.", redirectUrl: "/album" });
+    })
+    .catch(function (error) {
+      console.error("오류 발생:", error.message);
+      res.status(500).json({ error: "Failed to save user." });
+    });
+});
+
+// album 페이지 접근 시 로그인 상태 확인 후 처리
+app.get("/album", async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.send('<script>alert("로그인이 필요합니다."); window.location.href = "/";</script>');
+  }
+
+  try {
+    const user = await User.findOne({ email: req.user.email });
+    if (!user) {
+      return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+    }
+
+    // 사용자 모델에 'imagePaths'라는 필드가 이미지 URL 또는 경로를 저장하고 있다고 가정합니다.
+    const images = user.imagePaths; // 실제 모델에서 사용하는 필드명으로 'imagePaths' 대신 변경해주세요.
+
+    res.render("album.ejs", { images: images });
+  } catch (error) {
+    console.error("이미지 가져오기 에러:", error);
+    res.status(500).json({ error: "이미지를 가져오는데 실패했습니다." });
+  }
+});
+
+// 추억찾기 페이지 접근 시 로그인 상태 확인 후 처리
+app.get("/find", (req, res) => {
+  if (req.isAuthenticated()) {
+    // 로그인 상태라면 추억찾기 페이지로 이동
+    const currentUser = req.user;
+    res.render("find.ejs", { user: currentUser });
+  } else {
+    // 로그인되어 있지 않은 상태라면 알림창을 띄우고 로그인 페이지로 리디렉션
+    res.send('<script>alert("로그인이 필요합니다."); window.location.href = "/";</script>');
+  }
+});
+
+app.post("/find", uploadKeyImage.single("file_object"), async (req, res) => {
+  try {
+    // 업로드된 이미지 파일의 경로
+    const fileUrl = req.file.location;
+
+    // 사용자 정보
+    const user = req.user;
+    const userEmail = user.email;
+    const userImagePaths = user.imagePaths;
+
+    console.log("fileUrl : ", fileUrl);
+    console.log("req.user : ", req.user);
+
+    // 데이터를 Flask 서버로 전송
+    const data = {
+      fileUrl: fileUrl,
+      userEmail: userEmail,
+      userImagePaths: userImagePaths,
+    };
+    // Flask API의 URL
+    const FLASK_API_URL = "http://101.101.219.62:30006/search"; // 적절한 Flask API의 URL로 변경
+
+    axios
+      .post(FLASK_API_URL, data)
+      .then(function (response) {
+        console.log("Flask 애플리케이션으로부터 받은 응답:", response.data);
+        res.status(200).json(response.data);
+      })
+      .catch(function (error) {
+        console.error("Error while communicating with Flask server.:", error.message);
+        res.status(500).json({ error: "Failed to save user." });
+      });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Server error occurred." });
+  }
+});
+
+// 추억찾기 페이지 접근 시 로그인 상태 확인 후 처리
+app.get("/home", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/profile", (req, res) => {
+  // 현재 사용자 정보를 가져와서 프로필 페이지에 렌더링
+  if (req.isAuthenticated()) {
+    // 로그인 상태라면 추억찾기 페이지로 이동
+
+    const currentUser = req.user;
+    res.render("profile", { user: currentUser });
+  } else {
+    // 로그인되어 있지 않은 상태라면 알림창을 띄우고 로그인 페이지로 리디렉션
+    res.send('<script>alert("로그인이 필요합니다."); window.location.href = "/";</script>');
+  }
+});
+
+// 추억찾기 페이지 접근 시 로그인 상태 확인 후 처리
+app.get("/in", (req, res) => {
+  res.render("in.ejs");
+});
+
+// async function uploadImagePathsToFlaskAPI(email, imagePaths) {
+//   try {
+//     // Flask API의 엔드포인트 URL
+//     const apiUrl = "http://101.101.219.62:30006/find";
+
+//     // 요청할 데이터 생성
+//     const requestData = {
+//       email: email,
+//       imagePaths: imagePaths,
+//     };
+
+//     // Flask API에 POST 요청 보내기
+//     const response = await axios.post(apiUrl, requestData);
+
+//     // Flask API의 응답 처리
+//     if (response.status === 200) {
+//       console.log("이미지 경로들을 Flask API에 전송하였습니다.");
+//       console.log(response.data); // Flask API의 응답 데이터 확인
+//       return response.data;
+//     } else {
+//       console.error("Flask API 요청이 실패하였습니다.");
+//       return null;
+//     }
+//   } catch (error) {
+//     console.error("Flask API 요청 중 오류 발생:", error.message);
+//     return null;
+//   }
+// }
+
+// //get("/find") 라우트 핸들러 예시
+// app.get("/find", async (req, res) => {
+//   try {
+//     // req.user에서 email 가져오기 (이 부분은 실제로는 로그인을 통해 인증된 사용자 정보에서 가져오는 로직으로 대체해야 합니다.)
+//     const userEmail = req.user.email; // 사용자의 이메일 정보를 가져와서 저장합니다.
+
+//     try {
+//       const user = await User.findOne({ email: req.user.email });
+//       if (!user) {
+//         return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+//       }
+
+//       // 사용자 모델에 'imagePaths'라는 필드가 이미지 URL 또는 경로를 저장하고 있다고 가정합니다.
+//       const requestData = user.imagePaths; // 실제 모델에서 사용하는 필드명으로 'imagePaths' 대신 변경해주세요.
+
+//       // Flask API에 POST 요청 보내기
+//       const apiResponse = await uploadImagePathsToFlaskAPI(userEmail, requestData);
+
+//       // API 응답에 따라 처리
+//       if (apiResponse && apiResponse.message === "POST 요청이 성공적으로 처리되었습니다.") {
+//         // API 요청이 성공적으로 처리된 경우
+//         console.log("Flask API에서의 처리가 성공하였습니다.");
+//         // 다음 로직 추가: Flask API에서 반환한 결과를 이용하여 추가 처리
+//         res.status(200).json({ message: "Flask API 처리 성공" });
+//       } else {
+//         // API 요청이 실패한 경우 또는 API 응답에 에러 정보가 있는 경우
+//         console.error("Flask API에서 처리 실패 또는 오류 발생");
+//         res.status(500).json({ error: "Flask API 처리 실패" });
+//       }
+//     } catch (error) {
+//       console.error("이미지 가져오기 에러:", error);
+//       res.status(500).json({ error: "이미지를 가져오는데 실패했습니다." });
+//     }
+//   } catch (error) {
+//     console.error("에러 발생:", error.message);
+//     res.status(500).json({ error: "서버 에러 발생" });
+//   }
+// });
